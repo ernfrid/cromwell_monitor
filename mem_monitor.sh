5e4241feb0f9acc56b186695989798ab21a8fbbc
Dave Larson <delarson@wustl.edu>
2018-05-22 11:57:18 -0500

    Switch to tab-delimited output

--- mem_monitor.sh
+++ mem_monitor.sh
@@ -3,17 +3,18 @@
 set -ueo pipefail
 
 # Read memory stats for the machine from /proc/meminfo and convert from kB to mB.
-# Output a json object per timestamp
+# Output a tab-delimited line per timestamp
 
 function note_stats {
     local dt=$(date +'%Y-%m-%d %T')
     local memtotal=$(awk '/MemTotal/ {print $2 / 1024}' /proc/meminfo)
     local memfree=$(awk '/MemFree/ {print $2 / 1024}' /proc/meminfo)
     local memavailable=$(awk '/MemAvailable/ {print $2 / 1024}' /proc/meminfo)
-    echo -e "{ \"Timestamp\": \"${dt}\", \"MemTotal\": ${memtotal}, \"MemFree\": ${memfree}, \"MemAvail\": ${memavailable} }"
+    echo -e "${dt}\t${memtotal}\t${memfree}\t${memavailable}"
 }
 
-while true; do 
+echo -e "#Timestamp\tMemTotal\tMemFree\tMemAvail"
+while true; do
     note_stats
     sleep 60 
 done
